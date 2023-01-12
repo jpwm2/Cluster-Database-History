53c24c45ccdff0a961a35c7b4d2df0b039879301
jpwm20 <kohsaku.sub2@gmail.com>
2023-01-12 10:55:45 +0900

    history6

--- ANDB.py
+++ ANDB.py
@@ -1,58 +1,67 @@
 from sympy import isprime
 
-
-
 class DBServerBasic:
-    data = []
-    location = {}
+    data = {}
     def __init__(self):
-        getFile()
-
         def getFile():
             with open('ANDBData','r') as f:
                 self.data = eval(f.read())
-            with open('ANBDLocation','r') as f:
-                self.location = dict(f.read())
+        
+        getFile()
+
 
     def saveFile(self):
         with open('ANDBData','w') as f:
             f.write(str(self.data))
-        with open('ANDBLocation','w') as f:
-            f.write(str(self.location))
 
-    def doDBHaveObj(self,obj):
-        return True if obj in self.location.keys() else False
 
 class DBServer(DBServerBasic):
-    def areYouOwners(self, inObj, byOwners):
-        if self.doDBHaveObj(inObj):
-            owners = self.data[self.location.get(inObj)][1]
-            ownerNumber = len(owners)
-            count = 0
-            for id in byOwners.keys():
-                if id in owners.keys():
-                    if byOwners.get(id) in owners.values():
-                        count += 1
-            return True if ownerNumber // 2 < count else False
+    
+    def checkAndCreateObj(self,obj,objKey,owner,citeTo,citeFrom,data):
+        if obj not in self.data.keys():
+            self.data[obj] = [data,[objKey,[owner]],citeTo,citeFrom]
 
-    def doYouHaveOwners(self, toObj, fromObj, byOwners):
-        if ownerId in self.data[self.location.get(obj)][1].keys():
-            return True if ownerKey == self.data[self.location.get(obj)][1].get(ownerId) else False
+    def createObj(self,obj,objKey,owner,citeTo,citeFrom,data):
+        self.data[obj] = [data,[objKey,[owner]],citeTo,citeFrom]
+
+    def judgeYouOwners(self,obj,byOwners):
+        def operateOwner(self,obj,byOwners):
+            return True if self.data.get(obj)[1][0] == byOwners.get(obj) else False
+        if self.judgeDBHaveObj(obj):
+            owners = self.data.get(obj)[1][1]
+            ownerNumber = 1
+            ownerCount = 0
+            for o in owners:
+                ownerNumber += 1
+                ownerCount += 1 if self.judgeYouOwners(o,byOwners) else 0
+            if operateOwner(obj,byOwners):
+                ownerCount += 1
+            return True if ownerNumber // 2 <= ownerCount else False
+        else:
+            return False
+
+    def judgeYouRootOwner(self,rootKey):
+        return True if rootKey == 'rootpasskey1234' else False
 
     def judgeAccess(self, fromObj, toObj):
-        fromObj = int(fromObj)
-        toObj = int(toObj)
-        rootsFO = []
-        rootsTO = []
-        for path in self.data[self.location.get(fromObj)][2]:
-            rootsFO.append(path[0])
-        for path in self.data[self.location.get(toObj)][2]:
-            rootsTO.append(path[0])
-        for rfo in rootsFO:
-            for rto in rootsTO:
-                if rfo == rto:
-                    return True
-        return False
+        if self.judgeDBHaveObj(toObj):
+            if self.judgeDBHaveObj(fromObj):
+                fromObj = int(fromObj)
+                toObj = int(toObj)
+                rootsFO = []
+                rootsTO = []
+                for path in self.data.get(fromObj)[2]:
+                    rootsFO.append(path[0])
+                for path in self.data.get(toObj)[2]:
+                    rootsTO.append(path[0])
+                for rfo in rootsFO:
+                    for rto in rootsTO:
+                        if rfo == rto:
+                            return True
+                return False
+        else:
+            if self.judgeDBHaveObj(fromObj):
+
 
     def giveCites(self, fromObj, toObj):
         fromObj = int(fromObj)
@@ -60,81 +69,72 @@ class DBServer(DBServerBasic):
         giveCiteTo()
         giveCiteFrom()
         self.saveFile()
-
+        
         def giveCiteTo():
-            toObjPathes = self.data[self.location.get(toObj)][2]
-            fromObjPathes = self.data[self.location.get(fromObj)][2]
+            toObjPathes = self.data.get(toObj)[2]
+            fromObjPathes = self.data.get(fromObj)[2]
             for top in toObjPathes:
                 for fop in fromObjPathes:
                     if top != fop:
-                        self.lines[fromObj][3].append(top)
-
+                        self.data.get(fromObj)[2].append(top)
+        
         def giveCiteFrom():
-            self.lines[toObj][3].append(str(fromObj))
+            self.data.get(toObj)[3].append(str(fromObj))
 
     def changeData0(self, obj, data):
         data = str(data)
-        if self.doDBHaveObj(obj):
-            newLine = [data,{'8':'roootpassword'},[['8']],[]]
-            length = len(self.data)
-            self.data.append(newLine)
-            self.location[obj] = length
-        else:
-            self.data[self.location.get(obj)][0] = data
+        self.data.get(obj)[0] = data
 
     def addOwners0(self, toObj, fromObj):
-        added = True
-        for ownerId in self.data[self.location.get(fromObj)][1].keys():
-            if ownerId not in self.data[self.location.get(toObj)][1].keys():
-                self.data[self.location.get(toObj)][1][ownerId] = self.data[self.location.get(fromObj)][1].get(ownerId)
-                added = False
-        return added
+        if fromObj not in self.data.get(toObj)[1][1]:
+            self.data.get(toObj)[1][1].append(fromObj)
+            return True
+        else:
+            return False
         
-
-    def deleteOwner0(self, obj, ownerId):
-        del self.data[self.location.get(obj)][1][ownerId]
+    def deleteOwners0(self, toObj, fromObj):
+        self.data.get(toObj)[1][1].remove(fromObj)
 
 class ArrowNetworkDB(DBServer):
     def putAccessArrow(self, fromObj, byOwners, toObj):
-        if self.areYouOwners(fromObj, byOwners):
+        if self.judgeYouOwners(fromObj, byOwners):
             if self.judgeAccess(fromObj, toObj):
                 self.giveCites(fromObj, toObj)
 
+                
     def changeData(self, obj, byOwners,data):
-        if self.areYouOwners(obj, byOwners):
+        if self.judgeYouOwners(obj, byOwners):
             self.changeData0(obj,data)
 
-    def addOwners(self, toObj, fromObj, byOwners):
-        if self.areYouOwners(toObj, byOwners):
-            return self.addOwners0(toObj, fromObj)
 
+    def addOwners(self, toObj, fromObj, byOwners):
+        if self.judgeYouOwners(toObj, byOwners):
+            return self.addOwners0(toObj, fromObj)
+        else:
+            return False
+
+    
     def deleteOwner(self,toObj,fromObj,byOwners):
-        if self.doYouHaveOwners(toObj,byOwners):
+        if self.judgeYouOwners(toObj,byOwners):
             self.deleteOwner0(toObj,fromObj)
 
 class RootOwner():
-    iamtheOwner = {'8':'rootpassword'}
+    iamtheOwner = {'8':'rootpasskey1234'}
     def __init__(self):
         self.andb = ArrowNetworkDB()
-
-    def pickNumber(self):
-        return len(self.andb.data)
-            
-    def giveMeObj(self,byOwners,toObj, fromObj):
-        if isprime(self.length):
-            self.andb.changeData(self.length,self.iamtheOwner,self.length)
-            self.andb.putAccessArrow(self.length,self.iamtheOwner,'9')
+        
+    def giveMeObj(self, byOwners, toObj, fromObj):
+        if isprime(toObj):
+            self.andb.changeData(toObj,self.iamtheOwner,toObj)
+            self.andb.putAccessArrow(toObj,self.iamtheOwner,'9')
             return False
         else:
             if self.andb.addOwners(toObj,fromObj,byOwners):
+                self.andb.deleteOwner(toObj,'9',self.iamtheOwner)
+                return True
             else:
-                self.andb.deleteOwner(toObj,)
-
-    def createUser(self,data = None):
-        pass
-
-
+                return False
 
 
 r = RootOwner()
-r.giveMeObj({'8':'rootpassword'},'Way1')
+r.andb.
